8160e0577eec2c5394fb0b5440889aa46ba01130
Anna Qin <annaqinzhi@hotmail.com>
2022-03-02 18:56:12 +0100

    Update web.js

--- web.js
+++ web.js
@@ -8,27 +8,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const tag = req.body.fulfillmentInfo.tag;
-  let message = tag || req.query.message || req.body.message || 'Hello World!';
+  let message = "Hello World!";
   if (req.body.sessionInfo.parameters.age){
     message = "if you were 10 years older, you would be "+ (req.body.sessionInfo.parameters.age+10);
   }
+ 
   
-  const jsonResponse = {
-      fulfillment_response: {
-        messages: [
-          {
-            text: {
-              //fulfillment text response to be sent to the agent
-              text: [message],
-            },
-          },
-        ],
-      },
-
-  };
-  
-  res.status(200).send(jsonResponse);
+  res.status(200).send(message);
   
 });
 
