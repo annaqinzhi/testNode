7a86f3ba3b0123ed9633fee970ef50c077ef3d18
Anna Qin <annaqinzhi@hotmail.com>
2022-03-10 18:38:36 +0100

    Update web.js

--- web.js
+++ web.js
@@ -19,4 +19,27 @@ app.listen(port, () => {
 	console.log(`🌏 Server is running at http://localhost:${port}`)
 })
 
+app.post('/getage', (req, res) => {
+          const tag = req.body.fulfillmentInfo.tag;
+	  let message = tag || req.query.message || req.body.message || 'Hello World!';
+	  if (req.body.sessionInfo.parameters.ageinput){
+	    message = "if you were 10 years older, you would be "+ (req.body.sessionInfo.parameters.ageinput+10);
+	  }
+
+	  const jsonResponse = {
+	      fulfillment_response: {
+		messages: [
+		  {
+		    text: {
+		      //fulfillment text response to be sent to the agent
+		      text: [message],
+		    },
+		  },
+		],
+	      },
+
+	  };
+
+	  res.status(200).send(jsonResponse);
+})
 
