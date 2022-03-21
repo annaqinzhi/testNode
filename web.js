0c53c038e9dc2084fbfd94b6804b8983caee61eb
Anna Qin <annaqinzhi@hotmail.com>
2022-03-21 12:06:04 +0100

    Update web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ const http = require('http')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
