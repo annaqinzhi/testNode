97be9f1bbe86d66b70e293591a38093ebdcabf5d
Anna Qin <annaqinzhi@hotmail.com>
2022-03-28 14:39:40 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,9 @@
 const express = require('express')
 // will use this later to send requests
 const http = require('http')
-// import env variables
-require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -19,8 +17,7 @@ app.listen(port, () => {
 })
 
 app.post('/', function (req, res) {
-  const tag = req.body.fulfillmentInfo.tag;
-  let message = tag || req.query.message || req.body.message || 'Hello World!';
+let message = req.query.message || req.body.message || 'Hello World!';
   if (req.body.sessionInfo.parameters.age){
     message = "if you were 10 years older, you would be "+ (req.body.sessionInfo.parameters.age+10);
   }
@@ -38,7 +35,5 @@ app.post('/', function (req, res) {
       },
 
   };
-	
-  console.log("test Post tag" + tag);
   res.status(200).send(jsonResponse);
 })
